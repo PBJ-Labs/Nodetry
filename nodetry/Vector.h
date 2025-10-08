8de9a7ce61c4c96ec728442cdcdaa625ed2798c1
PB&J(*this)-sandwich <140750964+PBJ-Labs@users.noreply.github.com>
2025-10-08 08:28:23 -0500

    Create Vector.h

---
+++ nodetry/Vector.h
@@ -0,0 +1,19 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+using Vector = float;
+using Vector3 = float[3];
+using Vector4 = float[4];
+using Vector6 = float[6];
+
+template<typename T>
+T AddVectors( T va, T vb, T vc ){
+  return T(va + vb + vc);
+}
+
+template<typename T>
+T ReturnVector(T v){
+  return v;
+}
+
+#endif
