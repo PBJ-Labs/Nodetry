9a8f5deb5235793218f6b2c00c8ee09efe0908cc
PB&J(*this)-sandwich <140750964+PBJ-Labs@users.noreply.github.com>
2025-10-10 09:11:09 -0500

    Update plane.h

--- old-brushtypes/plane.h
+++ old-brushtypes/plane.h
@@ -2,10 +2,10 @@
 //plane.h
 //
 
-#ifndef PLANE
-#define PLANE
+#ifndef PLANE_H
+#define PLANE_H
 
-#define PLANE_EPSILON 00.1
+//#define PLANE_EPSILON 00.1
 
 typedef float vec_t;
 typedef vec_t vec3_t[3];
@@ -15,7 +15,6 @@ typedef struct plane_s{
    plane_s** next;
    plane_s* prev;
    plane_s owner;
-
    int points[3];
    int plnsize;
    double dist;
@@ -27,5 +26,10 @@ typedef struct plane_s{
    bool bChkOkay;
    const char* texture;
 }plane_t;
+
+plane_t* Alloc_Plane();
+plane_t* Create_Plane(vec3_t a, vec3_t b, vec3_t c, bool bOk, bool bInvert);
+
+void Free_Plane(plane_t * p);
 
 #endif
